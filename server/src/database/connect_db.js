03c06d3e8b72460e68640e0926aed97bed7cccd3
Matteo <matteonegridev@gmail.com>
2025-04-27 19:16:58 +0200

    -a

--- server/src/database/connect_db.js
+++ server/src/database/connect_db.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 const connectDB = (url) => {
   try {
-    mongoose.connect(url).then(() => console.log("database is connected"));
+    mongoose
+      .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+      .then(() => console.log("database is connected"));
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
   }
--- server/src/index.js
+++ server/src/index.js
@@ -29,7 +29,7 @@ app.use(
   session({
     saveUninitialized: false,
     resave: false,
-    secret: "recipeapp",
+    secret: process.env.SESSION_SECRET,
   })
 );
 app.use(cookieParser());
@@ -37,7 +37,7 @@ app.use(passport.initialize());
 
 app.use(
   cors({
-    origin: process.env.NODE_ENV === "production" ? "" : "http://localhost:5173",
+    origin: process.env.NODE_ENV === "production" ? process.env.CLIENT_URL : "http://localhost:5173",
     credentials: true,
   })
 );
